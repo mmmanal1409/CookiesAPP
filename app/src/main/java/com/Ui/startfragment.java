f1e5f1e3ec53f1bfbfc98c176503f6d1dabdd48a
fahdah <m_a_n1409@hotmail.com>
2022-08-28 07:22:51 +0300

    Initial commit

---
+++ app/src/main/java/com/Ui/startfragment.java
@@ -0,0 +1,4 @@
+package com.Ui;
+
+public class startfragment {
+}
---
+++ app/src/main/java/com/example/cookiesapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cookiesapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cookiesapp.R
+
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+}
+
+
+    }
+
+
+
+
+
+
+
